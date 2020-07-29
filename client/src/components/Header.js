2f5d50a50f95cbc4d74449d14649e29a3940d0b7
irjar <53143879+irjar@users.noreply.github.com>
2020-07-29 12:20:01 +0100

    remove logo image

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// Add a header on the login page and the register page
+function Header() {
+    return(
+            <div className="project-title">
+            <span className="h1">Dev Challenge</span>
+            </div>
+    )
+}
+export default Header;
